8d503d9bbb4d113596d882160455c889d70edc91
sulfidate <ffluid@me.com>
2021-05-06 20:08:06 +0000

    create pokemonList array and add three objects

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+	{ name: 'Bulbasaur', height: 0.7 , types: ['grass', 'poison']},
+	{ name: 'Butterfree', height: 1.1 , types: ['bug', 'flying']},
+	{ name: 'Pidgeot', height: 1.5 , types: ['flying', 'normal']}
+];
