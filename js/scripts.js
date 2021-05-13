8348784ee9cc02583c0dffb77ccc61423bcb982e
sulfidate <ffluid@me.com>
2021-05-13 20:49:37 +0000

    fix indention issues

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ let pokemonRepository = (function () {
 	];
 	// function to add anything to pokemonList within the repository / check type of item = object
 	function add(item){
-		if (typeof item === "object"){
+		if (typeof item === "object") {
 			pokemonList.push(item);
 		} else alert("not possible")
 	};
